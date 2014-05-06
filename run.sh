658f22fa382994f9ce57474d8d5061c8bfe22fbd
Bo Stendal Sørensen <bss@eflow.dk>
2014-05-06 10:57:09 +0700

    Remove cd step in run step
    The runstep contained a step that would basically cd out of the current dir. Which makes it not work, this is probably due to changes in the the dynamodb package from amazon.

--- run.sh
+++ run.sh
@@ -9,5 +9,4 @@ fi
 cd $WERCKER_STEP_ROOT
 wget http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest -O dynamodb_local.tar.gz
 tar xfz dynamodb_local.tar.gz
-cd $(tar tfz dynamodb_local.tar.gz | sed -n "1s/^\([^/]\+\).\+\$/\1/p")
 java -Djava.library.path=DynamoDBLocal_lib -jar DynamoDBLocal.jar -port $WERCKER_DYNAMODB_LOCAL_PORT &
